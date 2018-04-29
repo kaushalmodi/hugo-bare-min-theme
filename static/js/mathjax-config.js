9b1fcec350c0266be0d2b17cda0578489674b529
Kaushal Modi <kaushal.modi@gmail.com>
2018-04-29 13:40:57 -0400

    Add local js library copies # Better CSP compliance
    Changes in MathJax for better CSP compliance

---
+++ static/js/mathjax-config.js
@@ -0,0 +1,17 @@
+window.MathJax = {
+    displayAlign: "center",
+    displayIndent: "0em",
+    "HTML-CSS": { scale: 100,
+                  linebreaks: { automatic: "false" },
+                  webFont: "TeX"
+                },
+    SVG: {scale: 100,
+          linebreaks: { automatic: "false" },
+          font: "TeX"},
+    NativeMML: {scale: 100},
+    TeX: { equationNumbers: {autoNumber: "AMS"},
+           MultLineWidth: "85%",
+           TagSide: "right",
+           TagIndent: ".8em"
+         }
+};
