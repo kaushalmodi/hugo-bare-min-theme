cb656d41d8e97158421ba4b8f9bc8c72b29d63d6
Benjamin Ide <bcide@protonmail.com>
2019-10-27 00:39:13 -0400

    Update MathJax to v3

--- static/js/mathjax-config.js
+++ static/js/mathjax-config.js
@@ -1,17 +1,28 @@
 window.MathJax = {
+  chtml: {
     displayAlign: "center",
     displayIndent: "0em",
-    "HTML-CSS": { scale: 100,
-                  linebreaks: { automatic: "false" },
-                  webFont: "TeX"
-                },
-    SVG: {scale: 100,
-          linebreaks: { automatic: "false" },
-          font: "TeX"},
-    NativeMML: {scale: 100},
-    TeX: { equationNumbers: {autoNumber: "AMS"},
-           MultLineWidth: "85%",
-           TagSide: "right",
-           TagIndent: ".8em"
-         }
+    scale: 1
+  },
+  svg: {
+    scale: 1
+  },
+  tex: {
+    tags: "ams",
+    multlineWidth: "85%",
+    tagSide: "right",
+    tagIndent: ".8em",
+    autoload: {
+      color: [],
+      colorV2: ['color']
+    },
+    packages: {'[+]': ['noerrors']}
+  },
+  options: {
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process'
+  },
+  loader: {
+    load: ['[tex]/noerrors']
+  }
 };
