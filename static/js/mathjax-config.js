423093ee1692c3ceda3827b2ac7179cff10add02
Ben Ide <bcide@protonmail.com>
2019-10-27 20:32:53 +0000

    remove unnecessary options
    All of the removed options were autogenerated. It turns out that they were set to the default values and did not need to be included.

--- static/js/mathjax-config.js
+++ static/js/mathjax-config.js
@@ -1,28 +1,5 @@
 window.MathJax = {
-  chtml: {
-    displayAlign: "center",
-    displayIndent: "0em",
-    scale: 1
-  },
-  svg: {
-    scale: 1
-  },
   tex: {
-    tags: "ams",
-    multlineWidth: "85%",
-    tagSide: "right",
-    tagIndent: ".8em",
-    autoload: {
-      color: [],
-      colorV2: ['color']
-    },
-    packages: {'[+]': ['noerrors']}
-  },
-  options: {
-    ignoreHtmlClass: 'tex2jax_ignore',
-    processHtmlClass: 'tex2jax_process'
-  },
-  loader: {
-    load: ['[tex]/noerrors']
+    tags: "ams"
   }
 };
